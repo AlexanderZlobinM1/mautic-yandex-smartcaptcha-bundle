03ea3501e99f83d0324142e6f9e18bc48874426d
Alexander Zlobin <alexander.zlobin@m1.rs>
2026-07-06 15:28:15 +0200

    Add unified Sales Snap footer

--- Config/config.php
+++ Config/config.php
@@ -62,7 +62,7 @@ switch (true) {
 return [
     'name'        => 'Yandex SmartCaptcha',
     'description' => 'Adds a Yandex SmartCaptcha field and server-side validation to Mautic forms. Configure the Client key and Server key in the Auth tab.',
-    'version'     => '1.0.4',
+    'version'     => '1.0.5',
     'author'      => 'Sales Snap',
 
     'services' => [
